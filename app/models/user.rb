8a20bc2ebe88257d52a349999647f6cb60e4417a
joezini <josephlukeedwards@gmail.com>
2016-11-10 19:17:13 +0000

    complete exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, :email,  presence: true, length: { minimum: 5 }, uniqueness: true
-  has_many :posts, :comments
+  has_many :posts
+  has_many :comments
 end
